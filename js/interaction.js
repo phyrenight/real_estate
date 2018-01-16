25ee2f765fbcb2bda660591133e4233604ce0edf
Marc Preston <phyrenight@gmail.com>
2018-01-16 14:08:27 -0500

    add houses array

---
+++ js/interaction.js
@@ -0,0 +1,68 @@
+houses = [
+	{
+	  "main_pic" : "./images/",
+	  "inside_pic" : "./images/",
+	  "price": 200000,
+	  "address": "123 abc lane".
+	  "city": "",
+	  "state" :""
+	  "description":"",
+	  "num_of_rooms": 0,
+	  "num_of_bathrooms": 0
+	},
+	{
+	  "main_pic" : "./images/",
+	  "inside_pic" : "./images/",
+	  "price": 2000000,
+	  "address": "124 abc lane",
+	  "city": "",
+	  "state": "",
+	  "description":"",
+	  "num_of_rooms": 0,
+	  "num_of_bathrooms": 0
+	},
+  {
+	  "main_pic" : "./images/",
+	  "inside_pic" : "./images/",
+	  "price": 200000,
+	  "address": "125 abc lane",
+	  "city": "",
+	  "state": "",
+	  "description": "",
+	  "num_of_rooms": 0,
+	  "num_of_bathrooms": 0
+	},
+  {
+	  "main_pic" : "./images/",
+	  "inside_pic" : "./images/",
+	  "price": 2000000,
+	  "address": "126 abc lane",
+	  "city": "",
+	  "state": ""
+	  "description":,
+	  "num_of_rooms": 0,
+	  "num_of_bathrooms": 0
+	},
+  {
+	  "main_pic" : "./images",
+	  "inside_pic" : "./images/",
+	  "price": 2000000,
+	  "address": "127 abc lane",
+	  "city": "",
+	  "state":"",
+	  "description":
+	  "num_of_rooms": 0,
+	  "num_of_bathrooms": 0
+	},
+	{
+	  "main_pic" : "./images/,
+	  "inside_pic" : "./images/",
+	  "price": 2000000,
+	  "address": "128 abc lane",
+	  "city": "",
+	  "state":"",
+	  "description": ""
+	  "num_of_rooms": 0,
+	  "num_of_bathrooms": 0
+	}
+]
