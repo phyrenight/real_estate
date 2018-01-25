1f28164d7f7ba1c42c52640f4c046616c8840430
Marc Preston <phyrenight@gmail.com>
2018-01-25 15:46:54 -0500

    structure heading section for mobile

--- js/interaction.js
+++ js/interaction.js
@@ -7,9 +7,9 @@ houses = [
 	  "main_pic" : "./images/",
 	  "inside_pic" : "./images/",
 	  "price": 200000,
-	  "address": "123 abc lane".
+	  "address": "123 abc lane",
 	  "city": "",
-	  "state" :""
+	  "state" :"",
 	  "description":"",
 	  "num_of_rooms": 0,
 	  "num_of_bathrooms": 0
@@ -42,8 +42,8 @@ houses = [
 	  "price": 2000000,
 	  "address": "126 abc lane",
 	  "city": "",
-	  "state": ""
-	  "description":,
+	  "state": "",
+	  "description": "",
 	  "num_of_rooms": 0,
 	  "num_of_bathrooms": 0
 	},
@@ -54,18 +54,18 @@ houses = [
 	  "address": "127 abc lane",
 	  "city": "",
 	  "state":"",
-	  "description":
+	  "description": "",
 	  "num_of_rooms": 0,
 	  "num_of_bathrooms": 0
 	},
 	{
-	  "main_pic" : "./images/,
+	  "main_pic" : "./images/",
 	  "inside_pic" : "./images/",
 	  "price": 2000000,
 	  "address": "128 abc lane",
 	  "city": "",
 	  "state":"",
-	  "description": ""
+	  "description": "",
 	  "num_of_rooms": 0,
 	  "num_of_bathrooms": 0
 	}
@@ -73,14 +73,19 @@ houses = [
 
 function populateFeatureSection(){
 	// populates the feature section on the home page
-
+  let house = "";
 	let featureSection = document.getElementById("featureSection");
-  let array_length = houses.length;
+  let array_length = houses.length-1;
 	for(let i = 0; i < array_length; i++){
-    house = "<div onClick='' onHover=''><a href="+houses[i].["main_pic"]+">"+
-    houses[i]["address"]+" , "+ houses[i]["city"]+", "+houses[i]["state"]+
-    "</a><p>Price: "+houses[i]["price"]+"</p>"
-    "</div>";
+    house += "<div onClick='' onHover=''><a href='"+houses[i].main_pic+"'>"+
+    "number of bathrooms: "+ house[i].num_of_bathrooms +
+    "number of rooms: "+houses[i]["num_of_rooms"]+houses[i]["address"]+" , "+ houses[i]["city"]+", "+houses[i]["state"]+
+    "</a><p>Price: "+houses[i]["price"]+"</p></div>";
     featureSection.innerHTML = house;
 	}
+	console.log(house)
+	    featureSection.innerHTML = house;
 }
+
+console.log(window)
+window.onload = populateFeatureSection;
