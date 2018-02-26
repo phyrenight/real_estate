5fa191ded0e8e9c8c698622ab15bc39b887c2e25
Marc Preston <phyrenight@gmail.com>
2018-02-26 11:01:00 -0500

    style house listings

--- js/interaction.js
+++ js/interaction.js
@@ -8,8 +8,8 @@ houses = [
 	  "inside_pic" : "./images/",
 	  "price": 200000,
 	  "address": "123 abc lane",
-	  "city": "",
-	  "state" :"",
+	  "city": "wwwwwwww",
+	  "state" :"ww",
 	  "description":"",
 	  "num_of_rooms": 0,
 	  "num_of_bathrooms": 0
@@ -19,8 +19,8 @@ houses = [
 	  "inside_pic" : "./images/",
 	  "price": 2000000,
 	  "address": "124 abc lane",
-	  "city": "",
-	  "state": "",
+	  "city": "wwwwwww",
+	  "state": "ww",
 	  "description":"",
 	  "num_of_rooms": 0,
 	  "num_of_bathrooms": 0
@@ -30,8 +30,8 @@ houses = [
 	  "inside_pic" : "./images/",
 	  "price": 200000,
 	  "address": "125 abc lane",
-	  "city": "",
-	  "state": "",
+	  "city": "wwwwww",
+	  "state": "ww",
 	  "description": "",
 	  "num_of_rooms": 0,
 	  "num_of_bathrooms": 0
@@ -41,8 +41,8 @@ houses = [
 	  "inside_pic" : "./images/",
 	  "price": 2000000,
 	  "address": "126 abc lane",
-	  "city": "",
-	  "state": "",
+	  "city": "wwwwwww",
+	  "state": "ww",
 	  "description": "",
 	  "num_of_rooms": 0,
 	  "num_of_bathrooms": 0
@@ -52,8 +52,8 @@ houses = [
 	  "inside_pic" : "./images/",
 	  "price": 2000000,
 	  "address": "127 abc lane",
-	  "city": "",
-	  "state":"",
+	  "city": "wwwwww",
+	  "state":"ww",
 	  "description": "",
 	  "num_of_rooms": 0,
 	  "num_of_bathrooms": 0
@@ -63,8 +63,8 @@ houses = [
 	  "inside_pic" : "./images/",
 	  "price": 2000000,
 	  "address": "128 abc lane",
-	  "city": "",
-	  "state":"",
+	  "city": "wwwwwwww",
+	  "state":"ww",
 	  "description": "",
 	  "num_of_rooms": 0,
 	  "num_of_bathrooms": 0
@@ -77,12 +77,12 @@ function populateFeatureSection(){
 	let featureSection = document.getElementById("featureSection");
   let array_length = houses.length-1;
 	for(let i = 0; i < array_length; i++){
-    house += "<a href='#'><article onClick='' onHover=''><img src='"
-    +houses[i].main_pic+"'>"+houses[i]["address"]+" , "
-    +houses[i]["city"]+ ", " +houses[i]["state"]+
-    "number of bathrooms: "+ houses[i].num_of_bathrooms +
-    "number of rooms: "+houses[i]["num_of_rooms"]+
-    "<p>Price: "+houses[i]["price"]+"</p></article></a>";
+    house += "<a href='#'><article onClick='' onHover=''><div><img src='"
+    +houses[i].main_pic+"'></div><div><p>"+houses[i]["address"]+"</p><p> "
+    +houses[i]["city"]+ "</p><p> " +houses[i]["state"]+
+    "</p><p>number of bathrooms: "+ houses[i].num_of_bathrooms +
+    "</p><p>number of rooms: "+houses[i]["num_of_rooms"]+
+    "</p><p>Price: "+houses[i]["price"]+"</p></div></article></a>";
     featureSection.innerHTML = house;
 	}
 	    featureSection.innerHTML = house;
